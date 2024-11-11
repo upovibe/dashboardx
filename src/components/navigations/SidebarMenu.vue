7c6f851d4d6aa25048d5b7c3480ef043ac8fb9ff
PROMISE UZOR OKWUDIRI <158743974+upovibe@users.noreply.github.com>
2024-11-11 16:10:11 +0000

    Update SidebarMenu.vue

--- src/components/navigations/SidebarMenu.vue
+++ src/components/navigations/SidebarMenu.vue
@@ -14,6 +14,8 @@ type MenuItem = {
 const isCollapsed = ref(false);
 // State to track sidebar visibility
 const isVisible = ref(true);
+// State to track the currently active item
+const activeItem = ref<string | null>(null);
 
 function toggleSidebar() {
     isCollapsed.value = !isCollapsed.value;
@@ -30,95 +32,97 @@ defineProps<{
 </script>
 
 <template>
-    <!-- Conditionally render sidebar based on visibility -->
-    <div v-if="isVisible" :class="[ 
-        'bg-white h-screen p-5 rounded-3xl overfrlow-hidden flex flex-col transition-all duration-300 relative mr-5', 
-        isCollapsed ? 'w-20' : 'w-64' 
+  <!-- Conditionally render sidebar based on visibility -->
+  <div v-if="isVisible" :class="[
+        'bg-white h-screen p-5 rounded-3xl overflow-hidden flex flex-col transition-all duration-300 relative mr-5',
+        isCollapsed ? 'w-20' : 'w-64'
     ]">
-        <!-- Sidebar Header with Logo and Buttons -->
-        <div class="flex items-center justify-between mb-6">
-            <div v-if="!isCollapsed" class="flex items-center gap-5">
-                <Logo />
-            </div>
-            <button 
-                @click="toggleSidebar" 
-                class="rounded-xl hover:bg-gray-200 transition-all duration-200 p-1"
-                :class="{ 'mx-auto': isCollapsed }"
-            >
-                <img 
-                    src="../../assets/svgs/dashboard-svgrepo-com%20(1).svg" 
-                    alt="icon" 
-                    class="size-6"
-                />
-            </button>
-        </div>
-
-        <!-- Menu Items -->
-        <div class="flex-1">
-            <div v-for="(item, index) in items" :key="index" class="transition-opacity duration-300">
-                <!-- Render custom elements if provided, hide them when collapsed -->
-                <template v-if="item.customElements">
-                    <div v-if="!isCollapsed" class="my-3 text-sm text-gray-600" v-html="item.customElements"></div>
-                </template>
-
-                <!-- Menu Items with Custom Icon Component and Label -->
-                <template v-else>
-                    <a v-if="item.link" :href="item.link"
-                        class="flex items-center w-full font-semibold text-gray-700 no-underline gap-3 p-4 hover:bg-blue-600 hover:text-white duration-100 ease-linear hover:shadow-xl rounded-2xl cursor-pointer"
-                        :class="{ 'justify-center py-3': isCollapsed }"
-                    >
-                        <!-- Render custom icon component if provided -->
-                        <div v-if="item.customIcon">
-                            <component :is="item.customIcon" class="size-4" />
-                        </div>
-
-                        <!-- Show label only if sidebar is expanded -->
-                        <span v-if="!isCollapsed">{{ item.label }}</span>
-                    </a>
-                    <div v-else
-                        class="flex items-center w-full font-semibold text-gray-700 no-underline gap-3 p-4 hover:bg-blue-600 hover:text-white duration-100 ease-linear hover:shadow-xl rounded-2xl cursor-pointer"
-                        :class="{ 'justify-center': isCollapsed }"
-                    >
-                        <!-- Render custom icon component if provided -->
-                        <div v-if="item.customIcon">
-                            <component :is="item.customIcon" class="size-4" />
-                        </div>
-
-                        <!-- Show label only if sidebar is expanded -->
-                        <span v-if="!isCollapsed">{{ item.label }}</span>
-                    </div>
-                </template>
-            </div>
-        </div>
-
-        <!-- Custom Actions Slot -->
-        <div v-if="$slots.actions && !isCollapsed" class="mt-auto transition-opacity duration-300">
-            <slot name="actions"></slot>
-        </div>
+    <!-- Sidebar Header with Logo and Buttons -->
+    <div class="flex items-center justify-between mb-6">
+      <div v-if="!isCollapsed" class="flex items-center gap-5">
+        <Logo />
+      </div>
+      <button
+          @click="toggleSidebar"
+          class="rounded-xl hover:bg-gray-200 transition-all duration-200 p-1"
+          :class="{ 'mx-auto': isCollapsed }"
+      >
+        <img
+            src="../../assets/svgs/dashboard-svgrepo-com%20(1).svg"
+            alt="icon"
+            class="size-6"
+        />
+      </button>
     </div>
 
-    <!-- Button to hide/show sidebar, always on the right when collapsed -->
-    <div class="absolute top-1/2 left-0 transform -translate-y-1/2 z-50">
-        <!-- Show Chevron Right when sidebar is hidden -->
-        <button 
-            @click="toggleVisibility" 
-            v-if="!isVisible" 
-            class="transition-opacity duration-300 ease-linear"
-        >
-            <FaChevronRight class="text-gray-400 size-4" />
-        </button>
+    <!-- Menu Items -->
+    <div class="flex-1">
+      <div
+          v-for="(item, index) in items"
+          :key="index"
+          class="transition-opacity duration-300"
+          @click="setActiveItem(item.label)"
+      >
+        <!-- Render custom elements if provided, hide them when collapsed -->
+        <template v-if="item.customElements">
+          <div v-if="!isCollapsed" class="my-3 text-sm text-gray-600" v-html="item.customElements"></div>
+        </template>
 
-        <!-- Show Chevron Left when sidebar is collapsed -->
-        <button 
-            @click="toggleVisibility"
-            v-if="isVisible && isCollapsed"
-            class="transition-opacity duration-300 ease-linear"
-        >
-            <FaChevronLeft class="text-gray-400 size-4" />
-        </button>
+        <!-- Menu Items with Custom Icon Component and Label -->
+        <template v-else>
+          <a v-if="item.link" :href="item.link"
+             class="flex items-center w-full font-semibold text-gray-700 no-underline gap-3 p-4 hover:bg-blue-600 hover:text-white duration-100 ease-linear hover:shadow-xl rounded-2xl cursor-pointer"
+             :class="{ 'justify-center py-3': isCollapsed, 'active linked': activeItem === item.label }"
+          >
+            <!-- Render custom icon component if provided -->
+            <div v-if="item.customIcon">
+              <component :is="item.customIcon" class="size-4" />
+            </div>
+
+            <!-- Show label only if sidebar is expanded -->
+            <span v-if="!isCollapsed">{{ item.label }}</span>
+          </a>
+          <div v-else
+               class="flex items-center w-full font-semibold text-gray-700 no-underline gap-3 p-4 hover:bg-blue-600 hover:text-white duration-100 ease-linear hover:shadow-xl rounded-2xl cursor-pointer"
+               :class="{ 'justify-center': isCollapsed, 'active linked': activeItem === item.label }"
+          >
+            <!-- Render custom icon component if provided -->
+            <div v-if="item.customIcon">
+              <component :is="item.customIcon" class="size-4" />
+            </div>
+
+            <!-- Show label only if sidebar is expanded -->
+            <span v-if="!isCollapsed">{{ item.label }}</span>
+          </div>
+        </template>
+      </div>
     </div>
+
+    <!-- Custom Actions Slot -->
+    <div v-if="$slots.actions && !isCollapsed" class="mt-auto transition-opacity duration-300">
+      <slot name="actions"></slot>
+    </div>
+  </div>
+
+  <!-- Button to hide/show sidebar, always on the right when collapsed -->
+  <div class="absolute top-1/2 left-0 transform -translate-y-1/2 z-50">
+    <!-- Show Chevron Right when sidebar is hidden -->
+    <button
+        @click="toggleVisibility"
+        v-if="!isVisible"
+        class="transition-opacity duration-300 ease-linear"
+    >
+      <FaChevronRight class="text-gray-400 size-4" />
+    </button>
+
+    <!-- Show Chevron Left when sidebar is collapsed -->
+    <button
+        @click="toggleVisibility"
+        v-if="isVisible && isCollapsed"
+        class="transition-opacity duration-300 ease-linear"
+    >
+      <FaChevronLeft class="text-gray-400 size-4" />
+    </button>
+  </div>
 </template>
 
-<style scoped>
-/* Add any additional styles if needed */
-</style>
